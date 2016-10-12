31318a3c7fa4037670a22dfdaae81fef3b5ee58b
BJSS\kartik.prabhu <kartik.prabhu@bjss.com>
2016-10-12 09:48:33 +0100

    Added tests for ErrorResponse

--- mobile-connect-sdk/src/main/java/com/gsma/mobileconnect/r2/ErrorResponse.java
+++ mobile-connect-sdk/src/main/java/com/gsma/mobileconnect/r2/ErrorResponse.java
@@ -58,6 +58,14 @@ public class ErrorResponse
     }
 
     /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
      * @return the error URI.
      */
     public String getErrorUri()
