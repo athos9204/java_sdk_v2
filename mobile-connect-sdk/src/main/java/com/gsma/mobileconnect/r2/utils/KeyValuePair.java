99fab30a6b887bb26bc6c833817a3ac30e04d9d5
Furs, Anna <a.furs@a1qa.com>
2017-04-27 16:55:42 +0300

    Options without empty values

--- mobile-connect-sdk/src/main/java/com/gsma/mobileconnect/r2/utils/KeyValuePair.java
+++ mobile-connect-sdk/src/main/java/com/gsma/mobileconnect/r2/utils/KeyValuePair.java
@@ -143,6 +143,13 @@ public class KeyValuePair extends Tuple<String, String> implements NameValuePair
             return this;
         }
 
+        public ListBuilder addIfNotEmpty(final String key, final String value)
+        {
+            if (StringUtils.isNullOrEmpty(value))
+                this.list.add(new KeyValuePair(key, value));
+            return this;
+        }
+
         @Override
         public List<KeyValuePair> build()
         {
